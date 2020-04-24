424be4f7b8a66fae4ff582dee672e45fd3fb10cd
Your Name <v_dennis@126.com>
2020-04-24 09:37:11 +0800

    Initial commit

---
+++ NvFixInfo.py
@@ -0,0 +1,127 @@
+import re
+import os
+import sys
+import threading
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import messagebox
+import time
+
+
+class Config:
+    inf_name = r'/Display.Driver/nvcvi.inf'
+    a_file_path = ''
+    caution = "请在下方选择需要修改的驱动exe文件，选择后程序自动开始修改。"
+    Original_value = r'PCI\VEN_10DE&DEV_1C60&SUBSYS_6A031558'
+    Replacement_value = r'PCI\VEN_10DE&DEV_1C60&SUBSYS_74811558'
+    winrar = r'"c:/Program Files/WinRAR/WinRAR.exe"'
+    w = 800
+    h = 500
+
+
+window = tk.Tk()  # 实例化Gui库
+
+window.title('N卡驱动inf配置修改器')  # 定义ui窗口标题
+
+window.geometry('{}x{}'.format(Config.w, Config.h))  # 定义ui窗口大小
+
+
+# def callbackClose():
+#     os.system('bcdedit.exe /set nointegritychecks off')
+#     messagebox.showwarning(title='提示', message='关闭程序并且重新开启驱动强制签名')
+#     sys.exit(0)
+#
+#
+# window.protocol("WM_DELETE_WINDOW", callbackClose)
+
+
+def file_path():
+    Config.a_file_path = filedialog.askopenfilename()  # 弹窗选择文件
+    tk.Label(output, text='已选择文件：{}'.format(Config.a_file_path), wraplength=Config.w).grid(column=1, sticky='W')
+    # 定义一个线程用于抽奖中的效果
+    discoloration = threading.Thread(target=modification)
+    # 运行线程
+    discoloration.start()
+
+
+def installing_the_drive():
+    tk.Label(output, text='开始执行禁止驱动强制签名命令', wraplength=Config.w).grid(column=1, sticky='W')
+    if os.system('bcdedit.exe /set nointegritychecks on') == 0:
+        tk.Label(output, text='操作成功完成。', wraplength=Config.w).grid(column=1, sticky='W')
+        os.system(r'C:\Users\Administrator\Downloads\445.75-notebook-win10-64bit-international-dch-whql\setup.exe')
+        tk.Label(output, text='安装进程结束，重新开启驱动强制签名', wraplength=Config.w).grid(column=1, sticky='W')
+        if os.system('bcdedit.exe /set nointegritychecks off') == 0:
+            tk.Label(output, text='操作成功完成。', wraplength=Config.w).grid(column=1, sticky='W')
+        else:
+            tk.Label(output, text='开启驱动强制签名失败', wraplength=Config.w).grid(column=1,
+                                                                          sticky='W')
+    else:
+        tk.Label(output, text='禁用驱动强制签名失败，请开启禁用驱动签名模式后手动执行解压出来的驱动进行安装！', wraplength=Config.w).grid(column=1, sticky='W')
+
+
+file_selection = tk.Frame(window)  # 创建一个frame用于放置按钮
+
+file_selection.pack()  # 定义frame排列方式
+
+tk.Label(file_selection, text=Config.caution).grid(row=1, column=1)
+tk.Button(file_selection, text="点击开始选择", command=file_path).grid(row=2, column=1)
+
+output = tk.LabelFrame(window, text="log", labelanchor="n")  # 创建一个frame用于放置按钮
+
+output.pack()  # 定义frame排列方式
+
+
+def modification():
+    tk.Label(output, text='开始校验文件是不是exe文件', wraplength=Config.w).grid(column=1, sticky='W')
+    if Config.a_file_path.endswith('.exe'):  # 判断文件格式
+        tk.Label(output, text='文件校验：选择的文件是exe文件', wraplength=Config.w).grid(column=1, sticky='W')
+        b_file_path = Config.a_file_path.replace('/', '\\')  # cmd命令需要反斜杠，所以把文件目录斜杠替换掉
+
+        c_file_path = Config.a_file_path.rfind('.')  # 从右到左定位对应字符所在位置
+        c_file_path = Config.a_file_path[:c_file_path]  # 按指定位置截取字符串获得一个用来储存解压的文件夹
+
+        tk.Label(output, text='创建文件夹用于存放解压的驱动文件：{}'.format(c_file_path), wraplength=Config.w).grid(column=1, sticky='W')
+        if not os.path.exists(c_file_path):  # 如果文件夹不存在
+            os.makedirs(c_file_path)  # 创建文件夹
+
+        command = '{} x {} * {}'.format(Config.winrar, b_file_path, c_file_path)  # 组合cmd命令
+        tk.Label(output, text='开始执行解压命令：{}'.format(command), wraplength=Config.w, justify='left').grid(column=1,
+                                                                                                       sticky='W')
+        os.system(command)  # 执行cmd命令
+
+        d_file_path = c_file_path + Config.inf_name
+        tk.Label(output, text='查找inf文件：{}'.format(d_file_path), wraplength=Config.w).grid(column=1, sticky='W')
+        if re.search(Config.inf_name, d_file_path):
+            tk.Label(output, text='找到：{}'.format(d_file_path), wraplength=Config.w).grid(column=1, sticky='W')
+
+            tk.Label(output, text='开始编辑：{}'.format(d_file_path), wraplength=Config.w).grid(column=1, sticky='W')
+            infox = open(d_file_path, mode='tr')
+            data = infox.read()
+            tk.Label(output, text='查找inf文件内容：{}'.format(Config.Original_value), wraplength=Config.w).grid(column=1,
+                                                                                                          sticky='W')
+            finds = data.find(Config.Original_value)
+            if finds != -1:
+                data = data.replace(Config.Original_value, Config.Replacement_value)
+                conserve = open(d_file_path, mode='w', encoding='utf-8')
+                conserve.write(data)
+                conserve.close()
+                tk.Label(output, text='找到：{}，替换为：{}'.format(Config.Original_value, Config.Replacement_value),
+                         wraplength=Config.w).grid(column=1, sticky='W')
+
+                def installx():
+                    # 定义一个线程用于抽奖中的效果
+                    discoloration = threading.Thread(target=installing_the_drive)
+                    # 运行线程
+                    discoloration.start()
+
+                tk.Button(output, text="点击开始安装驱动", command=installx).grid(column=1)
+            else:
+                tk.Label(output, text='inf文件内找不到：{}'.format(Config.Original_value), wraplength=Config.w).grid(column=1,
+                                                                                                              sticky='W')
+        else:
+            tk.Label(output, text='没有找到inf文件！请确认选择的文件夹是否正确', wraplength=Config.w).grid(column=1, sticky='W')
+    else:
+        tk.Label(output, text='选择的文件不是exe文件！', wraplength=Config.w).grid(column=1, sticky='W')
+
+
+window.mainloop()
